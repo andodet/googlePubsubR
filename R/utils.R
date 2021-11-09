350254ea563aef44fca10a8a83eaf5a6df061452
andodet <an.dodet@gmail.com>
2021-11-09 18:12:54 +0100

    docs: add magrittr pipe to fix warning on solaris

--- R/utils.R
+++ R/utils.R
@@ -27,3 +27,17 @@ is.NullOb <- function(x) is.null(x) | all(sapply(x, is.null))
 secs_to_str <- function(x) {
   paste0(x, "s")
 }
+
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+#' @param lhs A value or the magrittr placeholder.
+#' @param rhs A function call using the magrittr semantics.
+#' @return The result of calling `rhs(lhs)`.
+NULL
