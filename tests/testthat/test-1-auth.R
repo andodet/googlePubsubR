78157b85d87aa7511fe24f6fb836dd6a211e1c43
andodet <an.dodet@gmail.com>
2021-12-18 20:53:21 +0100

    test: add tests for ps_project_get/set

--- tests/testthat/test-1-auth.R
+++ tests/testthat/test-1-auth.R
@@ -4,3 +4,15 @@ test_that("Auth works", {
 
   expect_true(googleAuthR::gar_has_token())
 })
+
+test_that("GCP project gets changed", {
+  project_id <- ps_project_set("testing")
+  get_proj <- ps_project_get()
+  
+  
+  expect_equal(project_id, "testing")
+  expect_equal(get_proj, "testing")
+  # Re-set it to env variable to keep on testing
+  on.exit(ps_project_set(Sys.getenv("GCP_PROJECT")))
+})
+
