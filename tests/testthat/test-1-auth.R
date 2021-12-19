f3a8301ee122c8b39f636df2082053327d29a761
andodet <an.dodet@gmail.com>
2021-12-19 11:15:00 +0100

    test: skip new tests on CI

--- tests/testthat/test-1-auth.R
+++ tests/testthat/test-1-auth.R
@@ -6,9 +6,11 @@ test_that("Auth works", {
 })
 
 test_that("GCP project gets changed", {
+  skip_on_cran()
+  skip_if_no_token()
+
   project_id <- ps_project_set("testing")
   get_proj <- ps_project_get()
-  
   
   expect_equal(project_id, "testing")
   expect_equal(get_proj, "testing")
@@ -17,5 +19,8 @@ test_that("GCP project gets changed", {
 })
 
 test_that("Setting GCP projectId as an empty string errors out", {
+  skip_on_cran()
+  skip_if_no_token()
+
   expect_error(ps_project_set(""))
 })
