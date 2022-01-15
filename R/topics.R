88c3092bbf7e8773c854e90f5f67d8b33242e4a7
andodet <an.dodet@gmail.com>
2022-01-15 23:06:56 +0100

    bug: delete name from topics_create body

--- R/topics.R
+++ R/topics.R
@@ -64,8 +64,7 @@ topics_create <- function(name,
     satisfies_pzs              = satisfies_pzs,
     message_storage_policy     = message_storage_policy,
     schema_settings            = schema_settings,
-    message_retention_duration = message_retention_duration,
-    name                       = topic_name
+    message_retention_duration = message_retention_duration
   )
 
   url <- sprintf("https://pubsub.googleapis.com/v1/%s", topic_name)
